b9fb0f2f1779eab6e02993256f33b9f6fcd89a59
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-07-19 02:46:29 +0200

    Posts management

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,13 +5,18 @@
 
 <body>
     <article>
-        <h1>My First Post</h1>
+        <h1><a href='/posts/my-first-post'>My First Post</a></h1>
         <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea eligendi qui aliquam dolorum assumenda reiciendis eveniet magni tempora sit nemo, quo enim similique quia totam, dignissimos sunt perspiciatis officia et!</p>
     </article>
 
     <article>
-        <h1>My Second Post</h1>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ea eligendi qui aliquam dolorum assumenda reiciendis eveniet magni tempora sit nemo, quo enim similique quia totam, dignissimos sunt perspiciatis officia et!</p>
+        <h1><a href='/posts/my-second-post'>My Second Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sed ullam necessitatibus, amet reprehenderit in culpa dignissimos odit, quam odio quia velit aliquid repellendus facilis quo sapiente. Sunt, fuga voluptatibus.</p>
+    </article>
+
+    <article>
+        <h1><a href='/posts/my-third-post'>My Third Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Pariatur cumque est quod voluptas sit tempore commodi officia id perspiciatis consequatur. Assumenda nobis deleniti hic ut obcaecati animi sit, at dignissimos.</p>
     </article>
 </body>
 
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('posts');
 });
+
+Route::get('posts/{post}', function($slug) {
+
+    $path = __DIR__ . "/../resources/posts/{$slug}.html";
+
+    // ddd($path);
+
+    if (!file_exists($path)) {
+        return redirect('/');
+        // ddd('File does not exist!');
+        // abort(404);
+    }
+    $post = file_get_contents($path);
+
+    return view('post', [
+        'post' => $post
+    ]);
+})->where('post', '[A-z_\-]+');
