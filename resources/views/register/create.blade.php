fd8e093340c479675c857a6466114f7307d41613
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-09-01 01:49:27 +0200

    Errors

--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -12,7 +12,11 @@
                            type="text"
                            name="name"
                            id="name"
+                           value="{{ old('name') }}"
                            required>
+                    @error('name')
+                        <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="username">
@@ -22,7 +26,11 @@
                            type="text"
                            name="username"
                            id="username"
+                           value="{{ old('username') }}"
                            required>
+                    @error('username')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="email">
@@ -32,7 +40,11 @@
                            type="email"
                            name="email"
                            id="email"
+                           value="{{ old('email') }}"
                            required>
+                    @error('email')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="password">
@@ -43,12 +55,22 @@
                            name="password"
                            id="password"
                            required>
+                    @error('password')
+                    <p class="text-red-500 text-xs mt-1">{{ $message }}</p>
+                    @enderror
                 </div>
                 <div class="mb-6">
                     <button type="submit" class="bg-blue-400 text-white rounded py-2 px-4 hover:bg-blue-500">
                         Submit
                     </button>
                 </div>
+{{--                @if ($errors->any())--}}
+{{--                    <ul>--}}
+{{--                        @foreach ($errors->all() as $error)--}}
+{{--                            <li class="text-red-500 text-xs">{{ $error }}</li>--}}
+{{--                        @endforeach--}}
+{{--                    </ul>--}}
+{{--                @endif--}}
             </form>
         </main>
     </section>
