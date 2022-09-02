0514e90651e0eb9346f8ef0dff377608be255fc5
Abdelrahman Hamdy <abdelrahmanhamdy49@gmail.com>
2022-09-03 00:40:41 +0200

    Form design and extracting a panel

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -50,6 +50,27 @@
                         {!! $post->body !!}
                     </div>
                 </div>
+                <x-panel>
+                    <form method="POST" action="#" class="mt-5">
+                        @csrf
+                        <header class="flex items-center">
+                            <img src="https://i.pravatar.cc/100?u={{ auth()->id() }}" alt="" width="40" height="60" class="rounded-full">
+                            <h2 class="ml-4">Want to participate?</h2>
+                        </header>
+
+                        <div class="mt-6">
+                            <label>
+                            <textarea name="body" rows="5" class="w-full text-sm focus:outline-none focus:ring"
+                                                    placeholder="Quick, think of something to say!">
+                            </textarea>
+                            </label>
+                        </div>
+
+                        <div class="flex justify-end mt-6 border-t border-gray-200 pt-6">
+                            <button type="submit" class="bg-blue-500 text-white uppercase font-semibold text-xs py-2 px-10 rounded-2xl hover:bg-blue-600">Post</button>
+                        </div>
+                    </form>
+                </x-panel>
                 <section class="col-span-8 col-start-5 mt-10 space-y-6">
                     @foreach ($post->comments as $comment)
                         <x-post-comment :comment="$comment"/>
